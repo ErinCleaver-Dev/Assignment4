7c86a23b53befabc26a5aa8bd092a5d1217bcaad
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-02-22 12:55:40 -0500

    Completed homework 2-22

--- Assignment4/Assignment4/Form1.Designer.vb
+++ Assignment4/Assignment4/Form1.Designer.vb
@@ -22,10 +22,136 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.bntAddNumber = New System.Windows.Forms.Button()
+        Me.NumberList = New System.Windows.Forms.ListBox()
+        Me.bntMean = New System.Windows.Forms.Button()
+        Me.bntMedian = New System.Windows.Forms.Button()
+        Me.bntMode = New System.Windows.Forms.Button()
+        Me.bntClearList = New System.Windows.Forms.Button()
+        Me.lblMean = New System.Windows.Forms.Label()
+        Me.lblMedian = New System.Windows.Forms.Label()
+        Me.lblMode = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(66, 162)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 23)
+        Me.txtNumber.TabIndex = 0
+        '
+        'bntAddNumber
+        '
+        Me.bntAddNumber.Location = New System.Drawing.Point(66, 206)
+        Me.bntAddNumber.Name = "bntAddNumber"
+        Me.bntAddNumber.Size = New System.Drawing.Size(100, 40)
+        Me.bntAddNumber.TabIndex = 1
+        Me.bntAddNumber.Text = "Add Number to List"
+        Me.bntAddNumber.UseVisualStyleBackColor = True
+        '
+        'NumberList
+        '
+        Me.NumberList.FormattingEnabled = True
+        Me.NumberList.ItemHeight = 15
+        Me.NumberList.Location = New System.Drawing.Point(205, 28)
+        Me.NumberList.Name = "NumberList"
+        Me.NumberList.Size = New System.Drawing.Size(120, 394)
+        Me.NumberList.TabIndex = 2
+        '
+        'bntMean
+        '
+        Me.bntMean.Location = New System.Drawing.Point(353, 79)
+        Me.bntMean.Name = "bntMean"
+        Me.bntMean.Size = New System.Drawing.Size(81, 43)
+        Me.bntMean.TabIndex = 3
+        Me.bntMean.Text = "Mean"
+        Me.bntMean.UseVisualStyleBackColor = True
+        '
+        'bntMedian
+        '
+        Me.bntMedian.Location = New System.Drawing.Point(353, 142)
+        Me.bntMedian.Name = "bntMedian"
+        Me.bntMedian.Size = New System.Drawing.Size(81, 43)
+        Me.bntMedian.TabIndex = 4
+        Me.bntMedian.Text = "Median"
+        Me.bntMedian.UseVisualStyleBackColor = True
+        '
+        'bntMode
+        '
+        Me.bntMode.Location = New System.Drawing.Point(353, 205)
+        Me.bntMode.Name = "bntMode"
+        Me.bntMode.Size = New System.Drawing.Size(81, 43)
+        Me.bntMode.TabIndex = 5
+        Me.bntMode.Text = "Mode"
+        Me.bntMode.UseVisualStyleBackColor = True
+        '
+        'bntClearList
+        '
+        Me.bntClearList.Location = New System.Drawing.Point(353, 328)
+        Me.bntClearList.Name = "bntClearList"
+        Me.bntClearList.Size = New System.Drawing.Size(81, 43)
+        Me.bntClearList.TabIndex = 6
+        Me.bntClearList.Text = "Clear List"
+        Me.bntClearList.UseVisualStyleBackColor = True
+        '
+        'lblMean
+        '
+        Me.lblMean.AutoSize = True
+        Me.lblMean.Location = New System.Drawing.Point(452, 93)
+        Me.lblMean.Name = "lblMean"
+        Me.lblMean.Size = New System.Drawing.Size(37, 15)
+        Me.lblMean.TabIndex = 7
+        Me.lblMean.Text = "Mean"
+        '
+        'lblMedian
+        '
+        Me.lblMedian.AutoSize = True
+        Me.lblMedian.Location = New System.Drawing.Point(452, 156)
+        Me.lblMedian.Name = "lblMedian"
+        Me.lblMedian.Size = New System.Drawing.Size(47, 15)
+        Me.lblMedian.TabIndex = 8
+        Me.lblMedian.Text = "Median"
+        '
+        'lblMode
+        '
+        Me.lblMode.AutoSize = True
+        Me.lblMode.Location = New System.Drawing.Point(452, 219)
+        Me.lblMode.Name = "lblMode"
+        Me.lblMode.Size = New System.Drawing.Size(38, 15)
+        Me.lblMode.TabIndex = 9
+        Me.lblMode.Text = "Mode"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(542, 450)
+        Me.Controls.Add(Me.lblMode)
+        Me.Controls.Add(Me.lblMedian)
+        Me.Controls.Add(Me.lblMean)
+        Me.Controls.Add(Me.bntClearList)
+        Me.Controls.Add(Me.bntMode)
+        Me.Controls.Add(Me.bntMedian)
+        Me.Controls.Add(Me.bntMean)
+        Me.Controls.Add(Me.NumberList)
+        Me.Controls.Add(Me.bntAddNumber)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents bntAddNumber As Button
+    Friend WithEvents NumberList As ListBox
+    Friend WithEvents bntMean As Button
+    Friend WithEvents bntMedian As Button
+    Friend WithEvents bntMode As Button
+    Friend WithEvents bntClearList As Button
+    Friend WithEvents lblMean As Label
+    Friend WithEvents lblMedian As Label
+    Friend WithEvents lblMode As Label
 End Class
--- Assignment4/Assignment4/Form1.vb
+++ Assignment4/Assignment4/Form1.vb
@@ -1,3 +1,104 @@
 ﻿Public Class Form1
 
+    Dim dbNumberList As Decimal()
+    Private Sub bntAddNumber_Click(sender As Object, e As EventArgs) Handles bntAddNumber.Click
+        Try
+            Dim dbNumber As Decimal = Decimal.Parse(
+            txtNumber.Text)
+            If NumberList.Items.Count < 20 Then
+                NumberList.Items.Add(dbNumber)
+            Else
+                MessageBox.Show("You can only enter 20 numbers. Please clear the list if you would like to start over.")
+            End If
+        Catch ex As FormatException
+            MessageBox.Show("Format Exception: " & ex.Message)
+        End Try
+
+        Try
+            dbNumberList = NumberList.Items.OfType(Of Decimal).ToArray()
+            Array.Sort(dbNumberList)
+
+
+        Catch
+            MessageBox.Show("Failed to convert the number item to a list")
+        End Try
+    End Sub
+
+    Private Sub bntMean_Click(sender As Object, e As EventArgs) Handles bntMean.Click
+        Try
+            Dim dbSum As Decimal = 0
+            For Each number In dbNumberList
+                dbSum += number
+            Next
+            Dim dbMean As Decimal = dbSum / dbNumberList.Length
+
+            lblMean.Text = dbMean
+
+
+        Catch ex As DivideByZeroException
+            MessageBox.Show("Divided By Zero Exception: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub bntMedian_Click(sender As Object, e As EventArgs) Handles bntMedian.Click
+        Try
+            Dim dbMedian As Decimal = 0
+            If dbNumberList.Length Mod 2 = 0 Then
+                Dim index As Integer = Math.Ceiling(Double.Parse(dbNumberList.Length) / 2) - 1
+                dbMedian = (dbNumberList(index) + dbMedian = dbNumberList(index + 1)) / 2
+            Else
+                Dim index As Integer = Math.Ceiling(Double.Parse(dbNumberList.Length) / 2) - 1
+                dbMedian = dbNumberList(index)
+            End If
+            lblMedian.Text = dbMedian
+        Catch ex As DivideByZeroException
+            MessageBox.Show("Divided By Zero Exception: " & ex.Message)
+        Catch ex As FormatException
+            MessageBox.Show("Format Exception: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub bntMode_Click(sender As Object, e As EventArgs) Handles bntMode.Click
+        Dim dbMode As Decimal
+        Dim dbTemp2 As Decimal
+        Dim dbCount As Integer = 0
+        Dim dbCount2 As Integer = 0
+        Try
+            For i As Integer = 0 To dbNumberList.Length - 1
+                If i = 0 Then
+                    dbMode = dbNumberList(i)
+                    dbCount += 1
+                ElseIf dbNumberList(i) = dbMode Then
+                    dbCount += 1
+                ElseIf dbNumberList(i) <> dbMode Then
+
+                    If dbCount2 = 0 Then
+                        Debug.WriteLine("Checking if db count is equal to 0")
+                        dbTemp2 = dbNumberList(i)
+                        dbCount2 += 1
+                    ElseIf dbCount2 > dbCount Then
+                        dbMode = dbTemp2
+                        dbCount = dbCount2
+                        dbCount2 = 0
+                    ElseIf dbTemp2 = dbNumberList(i) Then
+                        dbCount2 += 1
+                    Else
+                        dbCount2 = 1
+                        dbTemp2 = dbNumberList(i)
+                    End If
+                End If
+            Next
+            lblMode.Text = dbMode
+
+        Catch ex As IndexOutOfRangeException
+            MessageBox.Show("Index Out of Range Exception: " & ex.Message)
+        Catch ex As NullReferenceException
+            MessageBox.Show("NullReferenceException: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub bntClearList_Click(sender As Object, e As EventArgs) Handles bntClearList.Click
+        NumberList.Items.Clear()
+    End Sub
 End Class
